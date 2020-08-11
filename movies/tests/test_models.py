82fe0183c5aed454b268c1f972bf116b3de46c9b
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-08-11 21:35:04 +0300

    asd

--- movies/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from mixer.backend.django import mixer
-import pytest
-
-@pytest.mark.django_db
-class TestModels:
-
-    def test_movie_str(self):
-        movie = mixer.blend('movies.Movie', title = 'movie')
-        assert str(movie) == 'movie'
--- movies/tests/test_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse, resolve
-
-class TestUrls:
-
-    def test_movie_detail_url(self):
-        path = reverse('movie-detail', kwargs={'pk': 1})
-        assert resolve(path).view_name == 'movie-detail'
