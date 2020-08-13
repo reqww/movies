e9e94313b1e036b36cc1c80ab6e0e286f3e0d4f4
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-08-13 22:33:54 +0300

    docker+nginx done

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -155,9 +155,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
